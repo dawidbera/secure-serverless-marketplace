cabb8c645392110c110b6f634ccd69ce11be1237
dawidbera <10242059+dawidbera@users.noreply.github.com>
2026-02-08 19:06:59 +0000

    feat: setup java project with sam and localstack

---
+++ src/main/java/com/marketplace/products/GetProductsHandler.java
@@ -0,0 +1,22 @@
+package com.marketplace.products;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetProductsHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(200)
+                .withHeaders(headers)
+                .withBody("{\"message\": \"Hello from Secure Marketplace API!\"}");
+    }
+}
